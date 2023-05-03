8c9798bed96226b6fd2e1c60a238fcab9ea889a6
Esha019 <44088919+Esha019@users.noreply.github.com>
2023-05-03 11:21:24 +0530

    Added utils file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,8 @@
 const GOOGLE_API_KEY = "AIzaSyD9_VQKtPTJpPVCJRRRKYcNUrA1L7DwPv4";
 
+export const LIVE_CHAT_COUNT =  50;
+
 export const YOUTUBE_VIDEOS_API=
 "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=" + GOOGLE_API_KEY;
 
-export const testing= "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&myRating=like&key="+GOOGLE_API_KEY;
+export const YOUTUBE_SEARCH_API= "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
